723296d3b9d9eb08719a403ed1ba4f1b762c7b19
yuan <yuan.xiong@intel.com>
2019-03-02 21:51:45 -0500

    add tree code.

--- src/xyutils/tree/tree.cpp
+++ src/xyutils/tree/tree.cpp
@@ -2,7 +2,7 @@
 #include <unistd.h>
 #include <queue>
 
-#define N	137
+#define N	132
 using namespace std;
 
 struct ListNode {
@@ -13,9 +13,11 @@ struct ListNode {
 struct TreeNode {
 	int val;
 	int seq;
+	int leftHeight;
+	int rightHeight;
 	TreeNode *left;
 	TreeNode *right;
-	TreeNode(int x) : val(x), seq(-1), left(nullptr), right(nullptr) {}
+	TreeNode(int x) : val(x), seq(-1), leftHeight(0), rightHeight(0), left(nullptr), right(nullptr) {}
 };
 
 ListNode *lh;
@@ -46,23 +48,30 @@ void print_list() {
 
 int update_tree_seq(TreeNode *root, int shift) {
 
+	//cout << "update_tree_seq " << endl;
 	int n = shift;
+	if(root == nullptr) {
+		return n;
+	}
+
 	if(root->left) {
 		n = update_tree_seq(root->left, n);
 		n++;
 	}
-	if(root->right) {
-		update_tree_seq(root->right, n + 1);
-	}
-
-	cout << root->val << ":" << n << endl;
+	cout << root->val << ":" << n << " (" << root->leftHeight << ", " << root->rightHeight << ")" << endl;
+	//cout << "update_tree_seq " << endl;
 	root->seq = n;
+
+	if(root->right) {
+		n = update_tree_seq(root->right, n + 1);
+	}
 
 	return n;
 }
 
 void print_tree() {
 
+	//cout << "print_tree " << endl;
 	int blanks = 0;
 	update_tree_seq(th, 0);
 
@@ -90,6 +99,8 @@ void print_tree_hierarchy(TreeNode *root) {
 	if(root == nullptr) {
 		return;
 	}
+
+	//cout << "print_tree_hierarchy " << root->val << endl;
 
 	int currentLevel = 0;
 	int nextLevel = 0;
@@ -132,13 +143,51 @@ void print_tree_hierarchy(TreeNode *root) {
 	return;
 }
 
+int subtreeDepth(TreeNode *root) {
+
+	if(root == nullptr) {
+		return 0;
+	}
+
+	int leftHeight = 0;
+	int rightHeight = 0;
+
+	if(root->left) {
+		leftHeight = subtreeDepth(root->left);
+	}
+	if(root->right) {
+		rightHeight = subtreeDepth(root->right);
+	}
+
+	root->leftHeight = leftHeight;
+	root->rightHeight = rightHeight;
+
+	if(leftHeight == -1 || rightHeight == -1 || abs(leftHeight - rightHeight) > 1) {
+		return -1;
+	} else {
+		return leftHeight >= rightHeight ? leftHeight + 1 : rightHeight + 1;
+	}
+}
+
+bool isBalancedTree(TreeNode *root) {
+
+	cout << root->val << " Is balanced tree: ";
+
+	if(subtreeDepth(root) == -1) {
+		cout << "no" << endl;
+	} else {
+		cout << "yes" << endl;
+	}
+
+}
+
 void init() {
 
 	mylog("init");
 
 	ListNode *p = new ListNode(-1);
 	for(int i = 0; i < N; ++i) {
-		p->next = new ListNode(rand() % 10);
+		p->next = new ListNode(rand() % 100);
 		p = p->next;
 		if(lh == nullptr) {
 			lh = p;
@@ -172,9 +221,67 @@ int generate_tree() {
 	ListNode *p = lh;
 	while(p != nullptr) {
 		insert_list_to_tree(th, p->val);
+		isBalancedTree(th);
 		print_tree_hierarchy(th);
 		p = p->next;
 	}
+}
+
+vector<TreeNode *> generate_tree_all(int start, int end) {
+
+	vector<TreeNode *> subtree;
+
+	if(start > end) {
+		subtree.push_back(nullptr);
+		return subtree;
+	}
+
+	for(int i = start; i <= end; ++i) {
+
+		cout << "i: " << i << endl;
+
+		vector<TreeNode *> leftSubTree  = generate_tree_all(start, i - 1);
+		vector<TreeNode *> rightSubTree = generate_tree_all(i + 1, end);
+
+		for(auto left : leftSubTree) {
+			for(auto right : rightSubTree) {
+				TreeNode *root = new TreeNode(i);
+				root->left = left;
+				root->right = right;
+				subtree.push_back(root);
+
+				cout << "root: " << root->val << endl;
+				cout << "leftHeight: " << root->leftHeight << endl;
+				cout << "rightHeight: " << root->rightHeight << endl;
+				cout << "left: " << (left != nullptr) << endl;
+				cout << "right: " << (right != nullptr) << endl;
+				print_tree_hierarchy(root);
+			}
+		}
+	}
+
+	return subtree;
+}
+
+void generate_tree2(TreeNode*& root, int n) {
+
+	cout << "generate_tree2 for " << n << endl;
+	if(n <= 0) {
+		return;
+	}
+
+	generate_tree_all(1, n);
+
+	//int start = 1;
+	//int end = n;
+
+	//for(int i = start; i <= end; ++i) {
+	//	cout << "pivot: " << i << endl;
+	//	root = new TreeNode(i);
+	//	generate_tree_all(root->left, start, i - 1);
+	//	generate_tree_all(root->right, i + 1, end);
+	//	print_tree_hierarchy(th);
+	//}
 }
 
 int main(int argc, char** argv) {
@@ -183,6 +290,7 @@ int main(int argc, char** argv) {
 
 	init();
 
-	generate_tree();
+	//generate_tree();
+	generate_tree2(th, 3);
 	return 0;
 }
